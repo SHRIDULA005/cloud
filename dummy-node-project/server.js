220f283458750aadf5195fdec2b305b21d007358
SHRIDULA005 <shridula005@gmail.com>
2026-07-06 12:43:58 +0530

    Initial commit

--- dummy-node-project/server.js
+++ dummy-node-project/server.js
@@ -7,8 +7,6 @@ app.get("/", (req, res) => {
     res.send("Hello! This is my first GET API.");
 });
 
-const PORT = 3000;
-
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
