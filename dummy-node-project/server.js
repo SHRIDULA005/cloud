9ebc81d2580f420a53ca0e59954bc67cd3940f6e
SHRIDULA005 <shridula005@gmail.com>
2026-07-06 12:38:05 +0530

    Update server to listen on 0.0.0.0

--- dummy-node-project/server.js
+++ dummy-node-project/server.js
@@ -7,6 +7,8 @@ app.get("/", (req, res) => {
     res.send("Hello! This is my first GET API.");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+const PORT = 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
